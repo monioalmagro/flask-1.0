eeb5a8f48cc4a62f580c292728be74fe46918e41
emi <emazzurque@gmail.com>
2020-07-02 15:02:17 -0300

    urls

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from .views import page
+
 def create_app():
+    app.register_blueprint(page)
     return app
---
+++ app/views.py
@@ -0,0 +1,8 @@
+from flask import Blueprint
+from flask import render_template
+
+page = Blueprint('page',__name__)
+
+@page.route('/')
+def index ():
+    return render_template('index.html')
