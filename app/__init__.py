a7cb791fdd98e8aaf82b0400fecc475f1b271bb6
emi <emazzurque@gmail.com>
2020-07-06 00:54:54 -0300

    integrar bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap()
 
 from .views import page
 
 def create_app(config):
     app.config.from_object(config)
+
+    bootstrap.init_app(app)
+
     app.register_blueprint(page)
     return app
