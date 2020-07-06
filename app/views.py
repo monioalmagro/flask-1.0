781a0d91a3eccb1668a6a69c7cfa8059bbc0b925
emi <emazzurque@gmail.com>
2020-07-06 00:32:09 -0300

    errors

--- app/views.py
+++ app/views.py
@@ -3,6 +3,9 @@ from flask import render_template
 
 page = Blueprint('page',__name__)
 
+@page.app_errorhandler(404)
+def page_not_found(error):
+    return render_template('errors/404.html'), 404
 @page.route('/')
 def index ():
     return render_template('index.html')
